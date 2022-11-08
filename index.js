58bab233ae5a85f7e3244bce168d007957739aa2
Shakil <shakil.hossain450s@gmail.com>
2022-11-08 09:28:20 +0600

    simple server setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Service Review Server is running');
+});
+
+app.listen(port, () => {
+    console.log(`service review server listening on port ${port}`);
+})
