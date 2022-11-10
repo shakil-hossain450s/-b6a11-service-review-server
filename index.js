b846331cd8b7e2587fff9da540c376aa48bbd6d6
Shakil <shakil.hossain450s@gmail.com>
2022-11-10 20:39:49 +0600

    vercel config added

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bw7fh0s.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -52,11 +50,41 @@ async function run() {
             res.send(getSingleProduct);
         });
 
+        app.get('/my-reviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
 
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
         app.post('/reviews', async (req, res) => { 
             const reviews = req.body;
             const result = await reviewCollection.insertOne(reviews);
+            res.send(result);
+        })
+
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await foodCollection.insertOne(service);
+            res.send(result);
+        })
+
+        app.delete('/my-reviews/:id', async (req, res) => { 
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
             res.send(result);
         })
 
